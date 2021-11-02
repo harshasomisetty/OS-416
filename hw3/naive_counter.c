011632eab9291ba4e4ceecc7e6720db46ec78281
Shivam Agrawal <sa1547@ilab2.cs.rutgers.edu>
2021-11-01 21:57:02 -0400

    Completed implementation for naive counter.

--- hw3/naive_counter.c
+++ hw3/naive_counter.c
@@ -5,7 +5,41 @@
 
 #define COUNTER_VALUE (1UL << 24)
 
+int counter = 0;
+
+void* counterFunction(void* arg) {
+	int i = 0;
+	for (i = 0; i < COUNTER_VALUE; i++)
+		counter++;
+}
+
 int main(int argc, char** argv) {
 
+	if (argc < 2) {
+		printf("Error: empty thread count parameter\n");
+		abort();
+	}
+
+	int threadCount = atoi(argv[1]), i = 0;
+	pthread_t* threads = (pthread_t*) malloc(threadCount * sizeof(pthread_t));
+
+	struct timespec start, end;
+    clock_gettime(CLOCK_REALTIME, &start);
+	
+	for (i = 0; i < threadCount; i++) 
+		pthread_create(&(threads[i]), NULL, counterFunction, NULL);
+	
+	for (i = 0; i < threadCount; i++)
+		pthread_join(threads[i], NULL);
+
+	clock_gettime(CLOCK_REALTIME, &end);
+    printf("Counter finish in: %lu ms\n", (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000);
+
+	printf("The value of the counter should be: %ld\n", threadCount * COUNTER_VALUE);
+	printf("The value of the counter is: %d\n", counter);
+
+	free(threads);
 	return 0;
 }
+
+
