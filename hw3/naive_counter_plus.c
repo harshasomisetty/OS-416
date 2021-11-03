6147d773a84c18c05be0f7ce3c38857f2de84d14
Shivam Agrawal <sa1547@ilab1.cs.rutgers.edu>
2021-11-03 12:03:22 -0400

    Implemented naive counter plus.

--- hw3/naive_counter_plus.c
+++ hw3/naive_counter_plus.c
@@ -5,7 +5,48 @@
 
 #define COUNTER_VALUE (1UL << 24)
 
-int main(int argc, char** argv) {
+int counter = 0;
+pthread_mutex_t lock;
 
-	return 0;
+void* counterFunction(void* arg) {
+    int i = 0;
+    for (i = 0; i < COUNTER_VALUE; i++) {
+        pthread_mutex_lock(&lock);
+        counter++;
+        pthread_mutex_unlock(&lock);
+    }
+}
+
+int main(int argc, char** argv) {
+    
+    if (argc < 2) {
+        printf("Error: empty thread count parameter\n");
+        abort();
+    }
+    
+    int threadCount = atoi(argv[1]), i = 0;
+    pthread_t* threads = (pthread_t*) malloc(threadCount * sizeof(pthread_t));
+    pthread_mutex_init(&lock, NULL);
+    
+    struct timespec start, end;
+    clock_gettime(CLOCK_REALTIME, &start);
+    
+    for (i = 0; i < threadCount; i++)
+        pthread_create(&(threads[i]), NULL, counterFunction, NULL);
+    
+    for (i = 0; i < threadCount; i++)
+        pthread_join(threads[i], NULL);
+    
+    clock_gettime(CLOCK_REALTIME, &end);
+    
+    printf(
+           "Counter finish in: %lu ms\n",
+           (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000
+           );
+    printf("The value of the counter should be: %ld\n", threadCount * COUNTER_VALUE);
+    printf("The value of the counter is: %d\n", counter);
+    
+    pthread_mutex_destroy(&lock);
+    free(threads);
+    return 0;
 }
