aa17a159c03236855cd3691daf4fa99e05987e68
harshasomisetty <harshasomisetty7@gmail.com>
2021-12-13 23:54:02 -0500

    times of running

--- hw4/benchmark/bitmap_check.c
+++ hw4/benchmark/bitmap_check.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <sys/time.h>
 
 /* You need to change this macro to your TFS mount point*/
 #define TESTDIR "/tmp/hs884/mountdir"
@@ -30,15 +31,15 @@
 typedef unsigned char* bitmap_t;
 
 void set_bitmap(bitmap_t b, int i) {
-	b[i / 8] |= 1 << (i & 7);
+    b[i / 8] |= 1 << (i & 7);
 }
 
 void unset_bitmap(bitmap_t b, int i) {
-	b[i / 8] &= ~(1 << (i & 7));
+    b[i / 8] &= ~(1 << (i & 7));
 }
 
 uint8_t get_bitmap(bitmap_t b, int i) {
-	return b[i / 8] & (1 << (i & 7)) ? 1 : 0;
+    return b[i / 8] & (1 << (i & 7)) ? 1 : 0;
 }
 
 bitmap_t bitmap;
@@ -48,129 +49,139 @@ int diskfile = -1;
 
 int main(int argc, char **argv) {
 
-	int i, fd = 0, ret = 0;
-	int icount = 0, dcount = 0, icount_after = 0, dcount_after = 0;
-	struct stat st;
+    struct timeval tv;
+    struct timeval tv2;
+    
+    gettimeofday(&tv, NULL);
 
-	bitmap = malloc(BLOCKSIZE);
+    int i, fd = 0, ret = 0;
+    int icount = 0, dcount = 0, icount_after = 0, dcount_after = 0;
+    struct stat st;
 
-	diskfile = open("DISKFILE", O_RDWR, S_IRUSR | S_IWUSR);
-	if (diskfile < 0) {
-		perror("disk_open failed");
-		return -1;
-	}
+    bitmap = malloc(BLOCKSIZE);
 
-	// Read inode bitmap
-	ret = pread(diskfile, bitmap, BLOCKSIZE, I_BITMAP_BLOCK);
-	if (ret <= 0) {
-		perror("bitmap failed");
-	}
+    diskfile = open("DISKFILE", O_RDWR, S_IRUSR | S_IWUSR);
+    if (diskfile < 0) {
+        perror("disk_open failed");
+        return -1;
+    }
 
-	for (i = 0; i < MAX_INUM; ++i) {
-		if (get_bitmap(bitmap, i) == 1)
-			++icount;
-	}
+    // Read inode bitmap
+    ret = pread(diskfile, bitmap, BLOCKSIZE, I_BITMAP_BLOCK);
+    if (ret <= 0) {
+        perror("bitmap failed");
+    }
 
-	// Read data block bitmap
-	ret = pread(diskfile, bitmap, BLOCKSIZE, D_BITMAP_BLOCK);
-	if (ret <= 0) {
-		perror("bitmap failed");
-	}
+    for (i = 0; i < MAX_INUM; ++i) {
+        if (get_bitmap(bitmap, i) == 1)
+            ++icount;
+    }
 
-	for (i = 0; i < MAX_DNUM; ++i) {
-		if (get_bitmap(bitmap, i) == 1)
-			++dcount;
-	}
+    // Read data block bitmap
+    ret = pread(diskfile, bitmap, BLOCKSIZE, D_BITMAP_BLOCK);
+    if (ret <= 0) {
+        perror("bitmap failed");
+    }
+
+    for (i = 0; i < MAX_DNUM; ++i) {
+        if (get_bitmap(bitmap, i) == 1)
+            ++dcount;
+    }
 	
 
-	// Then do a set of operations
-	if ((ret = mkdir(TESTDIR "/testdir", DIRPERM)) < 0) {
-		perror("mkdir");
-		exit(1);
-	}
+    // Then do a set of operations
+    if ((ret = mkdir(TESTDIR "/testdir", DIRPERM)) < 0) {
+        perror("mkdir");
+        exit(1);
+    }
 
-	for (i = 0; i < N_FILES; ++i) {
-		char subdir_path[FSPATHLEN];
-		memset(subdir_path, 0, FSPATHLEN);
+    for (i = 0; i < N_FILES; ++i) {
+        char subdir_path[FSPATHLEN];
+        memset(subdir_path, 0, FSPATHLEN);
 
-		sprintf(subdir_path, "%s%d", TESTDIR "/testdir/dir", i);
-		if ((ret = mkdir(subdir_path, DIRPERM)) < 0) {
-			perror("mkdir");
-			exit(1);
-		}
+        sprintf(subdir_path, "%s%d", TESTDIR "/testdir/dir", i);
+        if ((ret = mkdir(subdir_path, DIRPERM)) < 0) {
+            perror("mkdir");
+            exit(1);
+        }
 
-		if ((ret = rmdir(subdir_path)) < 0) {
-			perror("rmdir");
-			exit(1);
-		}
-	}
+        if ((ret = rmdir(subdir_path)) < 0) {
+            perror("rmdir");
+            exit(1);
+        }
+    }
 
-	if ((fd = creat(TESTDIR "/test", FILEPERM)) < 0) {
-		perror("creat");
-		exit(1);
-	}
+    if ((fd = creat(TESTDIR "/test", FILEPERM)) < 0) {
+        perror("creat");
+        exit(1);
+    }
 
-	for (i = 0; i < ITERS; i++) {
-		//memset with some random data
-		memset(buf, 0x61 + i, BLOCKSIZE);
+    for (i = 0; i < ITERS; i++) {
+        //memset with some random data
+        memset(buf, 0x61 + i, BLOCKSIZE);
 
-		if (write(fd, buf, BLOCKSIZE) != BLOCKSIZE) {
-			perror("write");
-			exit(1);
-		}
-	}
+        if (write(fd, buf, BLOCKSIZE) != BLOCKSIZE) {
+            perror("write");
+            exit(1);
+        }
+    }
 
-	if (close(fd) < 0) {
-		perror("close");
-		exit(1);
-	}
+    if (close(fd) < 0) {
+        perror("close");
+        exit(1);
+    }
 
-	if ((ret = unlink(TESTDIR "/test")) < 0) {
-		perror("unlink");
-		exit(1);
-	}	
+    if ((ret = unlink(TESTDIR "/test")) < 0) {
+        perror("unlink");
+        exit(1);
+    }	
 
-	if ((ret = rmdir(TESTDIR "/testdir")) < 0) {
-		perror("rmdir");
-		exit(1);
-	}
+    if ((ret = rmdir(TESTDIR "/testdir")) < 0) {
+        perror("rmdir");
+        exit(1);
+    }
 
-	// Read inode bitmap
-	ret = pread(diskfile, bitmap, BLOCKSIZE, I_BITMAP_BLOCK);
-	if (ret <= 0) {
-		perror("bitmap failed");
-	}
+    // Read inode bitmap
+    ret = pread(diskfile, bitmap, BLOCKSIZE, I_BITMAP_BLOCK);
+    if (ret <= 0) {
+        perror("bitmap failed");
+    }
 
-	for (i = 0; i < MAX_INUM; ++i) {
-		if (get_bitmap(bitmap, i) == 1)
-			++icount_after;
-	}
+    for (i = 0; i < MAX_INUM; ++i) {
+        if (get_bitmap(bitmap, i) == 1)
+            ++icount_after;
+    }
 
-	memset(bitmap, 0, BLOCKSIZE);
+    memset(bitmap, 0, BLOCKSIZE);
 
-	// Read data block bitmap
-	ret = pread(diskfile, bitmap, BLOCKSIZE, D_BITMAP_BLOCK);
-	if (ret <= 0) {
-		perror("bitmap failed");
-	}
+    // Read data block bitmap
+    ret = pread(diskfile, bitmap, BLOCKSIZE, D_BITMAP_BLOCK);
+    if (ret <= 0) {
+        perror("bitmap failed");
+    }
 
-	for (i = 0; i < MAX_DNUM; ++i) {
-		if (get_bitmap(bitmap, i) == 1)
-			++dcount_after;
-	}
+    for (i = 0; i < MAX_DNUM; ++i) {
+        if (get_bitmap(bitmap, i) == 1)
+            ++dcount_after;
+    }
 	
-	if (abs(icount - icount_after) < ERR_THRESHOLD) {
-		printf("inodes reclaimed successfully %d\n", icount - icount_after);
-	} else {
-		printf("inodes reclaimed unsuccessfuly %d\n", icount - icount_after);
-	}
+    if (abs(icount - icount_after) < ERR_THRESHOLD) {
+        printf("inodes reclaimed successfully %d\n", icount - icount_after);
+    } else {
+        printf("inodes reclaimed unsuccessfuly %d\n", icount - icount_after);
+    }
 
-	if (abs(dcount - dcount_after) < ERR_THRESHOLD) {
-		printf("data blocks reclaimed successfully %d\n", dcount - dcount_after);
-	} else {
-		printf("data blocks reclaimed unsuccessfuly %d\n", dcount - dcount_after);
-	}
+    if (abs(dcount - dcount_after) < ERR_THRESHOLD) {
+        printf("data blocks reclaimed successfully %d\n", dcount - dcount_after);
+    } else {
+        printf("data blocks reclaimed unsuccessfuly %d\n", dcount - dcount_after);
+    }
 
-	printf("Benchmark completed \n");
-	return 0;
+    printf("Benchmark completed \n");
+
+    gettimeofday(&tv2, NULL);
+    double time = tv2.tv_usec - tv.tv_usec;
+    printf("Total Elapsed Time: %f microseconds\n", time);
+
+    return 0;
 }
--- hw4/run_test.sh
+++ hw4/run_test.sh
@@ -5,6 +5,12 @@ make >/dev/null
 cd benchmark
 make clean >/dev/null
 make >/dev/null
+rm -rf /tmp/hs884/mountdir/*
 ./simple_test
+rm -rf /tmp/hs884/mountdir/*
+./test_case
+rm -rf /tmp/hs884/mountdir/*
+./bitmap_check
+rm -rf /tmp/hs884/mountdir/*
 cd ..
 rm *.o
